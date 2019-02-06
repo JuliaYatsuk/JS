24289eddce5768c6a20c6bfce141ee83a7037b6e
Jus <jus2006@ukr.net>
2019-02-06 09:58:38 +0100

    Telephon Number

---
+++ myFilter.js
@@ -0,0 +1,19 @@
+
+var s = [23, 65, 98, 5];
+
+Array.prototype.myFilter = function(callback){
+  var newArray = [];
+  
+  this.forEach(function(a){
+     if (callback(a)) newArray.push(a)
+         });
+  
+  return newArray;
+
+};
+
+var new_s = s.myFilter(function(item){ 
+  return item % 2 === 1;
+});
+
+console.log(new_s);
